8ac772b081ede7177bf15499cd98677bdfc5d1a6
angeloey <angelomaoudis@gmail.com>
2024-03-10 14:26:14 +0000

    started potentiometer library

---
+++ My-Pot-Lib/pot.cpp
@@ -0,0 +1,17 @@
+// Angelo Potentiometer
+#include "pot.h"
+
+Pot::Pot(PinName potOut):voltIn(potOut){
+    voltage = voltIn.read();
+}
+
+    // Returns AnalogIn voltage on pin potOut
+float Pot::readVoltage(void){
+    voltage = voltIn.read() * 3.3;
+    return voltage;
+}
+
+    // Returns last measured reading, without measuring a new one
+float Pot::getLastReading(void){
+    return voltage;
+}
---
+++ My-Pot-Lib/pot.h
@@ -0,0 +1,18 @@
+// Angelo Potentiometer
+#include "mbed.h"
+
+class Pot{
+public:
+    Pot(PinName potOut);
+
+    // Returns AnalogIn voltage on pin potOut
+    float readVoltage();
+
+    // Returns last measured reading, without measuring a new one
+    float getLastReading();
+    
+private:
+    float voltage;
+
+    AnalogIn voltIn;
+};
